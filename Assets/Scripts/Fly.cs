715ba7b044f2dbc877161f63d65ff1b3297a76e7
Zak Lam Tam <tamusername@gmail.com>
2022-01-15 14:34:16 -0500

    smoother vehicle control

--- Assets/Scripts/Fly.cs
+++ Assets/Scripts/Fly.cs
@@ -7,20 +7,14 @@ public class Fly : MonoBehaviour
     private bool _locked = false;
 
     private const float _tiltScale = 0.5f;
-    private const float _forwardScale = 0.01f;
-    private Vector3 _eulerRotate;
-
-    //private float _forwardInput;
-    //private float _rotationInput;
-    //private Vector3 _userRot;
+    private const float _forwardScale = 50f;
 
     private Rigidbody _rigidbody;
-    private Transform _transform;
 
     // Start is called before the first frame update
     void Start()
     {
-        _transform = GetComponent<Transform>();
+        
     }
 
     // Update is called once per frame
@@ -28,31 +22,11 @@ public class Fly : MonoBehaviour
     {
         if (!_locked)
         {
-            _transform.position += _forwardScale * _transform.forward;
-
-            _transform.Rotate(_tiltScale*Input.GetAxis("Vertical"), 0, -_tiltScale*Input.GetAxis("Horizontal"));
+            // forward
+            transform.position += _forwardScale * transform.forward * Time.deltaTime;
+            // tilting
+            transform.Rotate(_tiltScale*Input.GetAxis("Vertical"), 0, -_tiltScale*Input.GetAxis("Horizontal"));
         }
-        
-
-        // NEW
-        //_eulerRotate = _transform.rotation.eulerAngles;
-        //_eulerRotate += new Vector3(0, Input.GetAxis("Horizontal"), 0);
-        //_transform.rotation = Quaternion.Euler(_eulerRotate);
-
-        //_transform.Rotate(_tiltScale * Input.GetAxis("TiltVertical"), 0, -_tiltScale * Input.GetAxis("TiltHorizontal"));
-
-        // avoid go below the terrrain
-        // float currAltitude = Terrain.activeTerrain.SampleHeight(_transform.position);
-        // if (currAltitude > _transform.position.y)
-        // {
-        //     _transform.position = new Vector3(_transform.position.x,
-        //         currAltitude,
-        //         _transform.position.z);
-        // }
-        //_transform.position += new Vector3(
-        //    _transform.forward.x * Input.GetAxis("Vertical") * _forwardScale,
-        //    _transform.forward.y * Input.GetAxis("TiltVertical") * _forwardScale,
-        //    _transform.forward.z * Input.GetAxis("Vertical") * _forwardScale);
     }
 
     public void Lock()
